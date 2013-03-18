09d284494d24a74741752bef4928e6f049dddee8
Ivan Petroye <petroei@gmail.com>
2013-03-18 11:31:42 +0200

    added ipv4/ipv6 validation

--- Gemfile
+++ Gemfile
@@ -2,4 +2,9 @@ source 'https://rubygems.org'
 
 # Specify your gem's dependencies in spamhaus_checker.gemspec
 gem 'net-dns'
+gem 'ipaddress'
+group :test do
+  gem 'rspec', :require => false
+  gem 'debugger'
+end
 gemspec
--- lib/spamhaus_checker/zen.rb
+++ lib/spamhaus_checker/zen.rb
@@ -1,9 +1,11 @@
 require 'rubygems'
 require 'net/dns'
+require 'ipaddress'
 module SpamhausChecker
   class ZEN
     attr_reader :server_ip, :answer
     def initialize(server_ip, zen_domain= "zen.spamhaus.org")
+      raise ArgumentError, 'Argument is not valid IP Address' unless  IPAddress.valid? server_ip
       @server_ip = server_ip
       resolver = Net::DNS::Resolver.new( :dns_search => false)
       reversed_ip = server_ip.to_s.split(".").reverse.join(".")
